bbb77e5a2fe612abcd399947ec184760a53bc180
Holly Tancredi <tancredi.holly@gmail.com>
2015-11-17 17:22:11 -0500

    basics

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,29 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
+$(document).ready(function() {
+    var audioSection = $('section#song');
+    $('a.play_song').click(function() {
+        var audio = $('<audio>', {
+            controls : 'controls'
+    });
+    var url = $(this).attr('href');
+    $('<source>').attr('src', url).appendTo(audio);
+    audioSection.html(audio);
+    return false;
+    });
+});
